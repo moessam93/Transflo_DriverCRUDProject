5e4301a809a1c4716d9b09f1f5fb79781a5b1b4d
Mohamed Essam <mohamedessam.extend@outlook.com>
2023-05-19 12:16:35 +0300

    Add code comments to SeedDrivers program

--- SeedDriversData/Program.cs
+++ SeedDriversData/Program.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Numerics;
 
 class Program
 {
@@ -10,12 +10,13 @@ class Program
 
     static void Main(string[] args)
     {
-
+        // Configuring the application settings using the appsettings.json file
         IConfiguration configuration = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
             .AddJsonFile("appsettings.json")
             .Build();
 
+        // Retrieving the connection string from the configuration
         connectionString = configuration.GetConnectionString("DefaultConnection");
 
         Console.WriteLine("Welcome to the Data Seeding Program!");
@@ -27,6 +28,7 @@ class Program
         {
             try
             {
+                // Calling the SeedDrivers method to seed data into the database
                 SeedDrivers();
 
                 Console.WriteLine("Data seeding operation completed successfully!");
@@ -47,56 +49,68 @@ class Program
 
     public static void SeedDrivers()
     {
+        // Establishing a connection to the database using the connection string
         using (var connection = new SqlConnection(connectionString))
         {
             connection.Open();
 
+            // Creating a SQL command object to execute the data seeding query
             SqlCommand command = new SqlCommand();
 
             command.Connection = connection;
             command.CommandType = CommandType.Text;
-            command.CommandText = @"WITH Numbers AS (
-    SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNumber
-    FROM sys.all_objects
-)
-INSERT INTO Drivers (FirstName, LastName, Email, PhoneNumber)
-SELECT
-    CHAR(65 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) AS FirstName,
-    CHAR(65 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) AS LastName,
-    CONCAT(CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
-    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) , '@transflo.com') AS Email,
-    CAST(ABS(CHECKSUM(NEWID())) AS INT) AS PhoneNumber
-FROM Numbers
-WHERE RowNumber <= 100;";
 
+            // Setting the data seeding query to insert random driver data into the Drivers table
+            command.CommandText = @"
+                WITH Numbers AS (
+                    SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNumber
+                    FROM sys.all_objects
+                )
+                INSERT INTO Drivers (FirstName, LastName, Email, PhoneNumber)
+                SELECT
+                    -- Generating a random first name
+                    CHAR(65 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) AS FirstName,
+
+                    -- Generating a random last name
+                    CHAR(65 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) AS LastName,
+
+                    -- Generating an email in the format randomString@transflo.com
+                    CONCAT(CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) +
+                    CHAR(97 + (ABS(CHECKSUM(NEWID())) % 26)) , '@transflo.com') As Email,
+
+                    --Generating a random phone number
+                    CAST(ABS(CHECKSUM(NEWID())) AS INT) AS PhoneNumber
+                FROM Numbers
+                WHERE RowNumber <= 100;";
+
+            // Executing the data seeding query and returning the number of rows affected
             int rowsAffected = command.ExecuteNonQuery();
         }
-
     }
 }
